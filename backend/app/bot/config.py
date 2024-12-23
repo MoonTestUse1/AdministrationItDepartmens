7e35c2f318d4cc7196f3bd44e2646bcb534ea294
MoonTestUse1 <crocoman7887@gmail.com>
2024-12-23 22:31:14 +0600

    tg

--- backend/app/bot/config.py
+++ backend/app/bot/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    TELEGRAM_BOT_TOKEN: str = "7677506032:AAHoVqFJs3IZKNK2NVzGnzKUn1hjVtU5Ryk"
+    TELEGRAM_BOT_TOKEN: str = "7677506032:AAHB2QtrxKdgUXLWlE2xXaVxs9V7BPz1fhc"
     TELEGRAM_CHAT_ID: str = "5057752127"
 
     class Config:
