38df860fe377f0064dd2fe6a5891c3f0f681c3bb
MoonTestUse1 <crocoman7887@gmail.com>
2024-12-25 03:02:17 +0600

    Merge remote changes and resolve conflicts

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-# Остановить все контейнеры
-docker compose down -v
+domains=(itformhelp.ru www.itformhelp.ru)
+email="admin@itformhelp.ru"
+staging=0 # Set to 1 if you're testing your setup
 
-# Создать необходимые директории
-mkdir -p ./certbot/www
-mkdir -p ./certbot/conf
+# Create dummy certificates
+path="/etc/letsencrypt/live/$domains"
+docker compose run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:4096 -days 1\
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
 
-# Запустить только nginx для первичной проверки
-docker compose up -d frontend
+echo "### Starting nginx ..."
+docker compose up --force-recreate -d frontend
 
-# Подождать, пока nginx запустится
-sleep 5
+echo "### Deleting dummy certificate ..."
+docker compose run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$domains && \
+  rm -Rf /etc/letsencrypt/archive/$domains && \
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
 
-# Запустить certbot для получения сертификата
-docker compose run --rm certbot
+echo "### Requesting Let's Encrypt certificate ..."
+docker compose run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/html \
+    --email $email \
+    --agree-tos \
+    --no-eff-email \
+    --force-renewal \
+    ${staging:+--staging}" certbot
 
-# Перезапустить все сервисы
-docker compose down
-docker compose up -d
+echo "### Reloading nginx ..."
+docker compose exec frontend nginx -s reload
