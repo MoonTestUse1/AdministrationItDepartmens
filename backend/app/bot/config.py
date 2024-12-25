7034c1ce54eaf4a3d4c0b28c6f8f2900b1de201b
1 <crocoman7887@gmail.com>
2024-12-25 14:06:56 +0600

    Cейчас точно заработает3

--- backend/app/bot/config.py
+++ backend/app/bot/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     TELEGRAM_BOT_TOKEN: str = "7677506032:AAHduD5EePz3bE23DKlo35KoOp2_9lZuS34"
-    TELEGRAM_CHAT_ID: str = "5057752127"
+    TELEGRAM_CHAT_ID: int = 5057752127
 
     class Config:
         env_file = ".env"
