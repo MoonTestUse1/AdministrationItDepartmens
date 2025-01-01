91c43a8adc31543b989212cd0f4eda3a5b3d2ebd
MoonTestUse1 <crocoman7887@gmail.com>
2025-01-02 05:03:51 +0600

    Добавления тестов бекенда3

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,11 @@
 """Database configuration"""
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres123@postgres:5432/support_db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}  # only needed for SQLite
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
