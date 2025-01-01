2c3d722f579b42d0185bf43d6d5cb4a919b5db27
MoonTestUse1 <crocoman7887@gmail.com>
2025-01-02 01:10:12 +0600

    чиним билд113

--- backend/app/schemas/tables.py
+++ backend/app/schemas/tables.py
@@ -10,17 +10,3 @@ class RequestStatus(str, enum.Enum):
     in_progress = "in_progress"
     resolved = "resolved"
     closed = "closed"
-
-class Request(Base):
-    __tablename__ = "requests"
-
-    id = Column(Integer, primary_key=True, index=True)
-    employee_id = Column(Integer, ForeignKey("employees.id"))
-    department = Column(String, nullable=False)
-    request_type = Column(String, nullable=False)
-    priority = Column(String, nullable=False)
-    description = Column(String, nullable=False)
-    status = Column(Enum(RequestStatus), default=RequestStatus.new)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-
-    employee = relationship("Employee", back_populates="requests")
