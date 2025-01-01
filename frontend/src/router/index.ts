00a035653f7c4aa3906e991bc8a14ad00aecd5b8
MoonTestUse1 <crocoman7887@gmail.com>
2025-01-01 22:53:45 +0600

    Починка админки полностью1д

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,51 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import { useAuthStore } from '@/stores/auth';
-import LoginView from '../views/LoginView.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/support',
-      name: 'support',
-      component: () => import('../views/SupportView.vue'),
-      meta: { requiresAuth: true }
-    },
-    {
       path: '/admin',
-      name: 'admin-login',
-      component: () => import('../views/admin/AdminLoginView.vue')
+      component: () => import('@/layouts/AdminLayout.vue'),
+      children: [
+        {
+          path: '',
+          redirect: '/admin/dashboard'
+        },
+        {
+          path: 'dashboard',
+          name: 'AdminDashboard',
+          component: () => import('@/views/admin/DashboardView.vue')
+        },
+        {
+          path: 'requests',
+          name: 'AdminRequests',
+          component: () => import('@/views/admin/RequestsView.vue')
+        },
+        {
+          path: 'employees',
+          name: 'AdminEmployees',
+          component: () => import('@/views/admin/EmployeesView.vue')
+        },
+        {
+          path: 'employees/add',
+          name: 'AdminEmployeeAdd',
+          component: () => import('@/views/admin/AddEmployeeView.vue')
+        }
+      ]
     },
     {
-      path: '/admin/dashboard',
-      name: 'admin-dashboard',
-      component: () => import('../views/admin/DashboardView.vue'),
-      meta: { requiresAdmin: true }
-    },
-    {
-      path: '/admin/employees/add',
-      name: 'add-employee',
-      component: () => import('../views/admin/AddEmployeeView.vue'),
-      meta: { requiresAdmin: true }
+      path: '/admin/login',
+      name: 'AdminLogin',
+      component: () => import('@/views/admin/AdminLoginView.vue')
     }
   ]
-});
-
-router.beforeEach((to, _, next) => {
-  const authStore = useAuthStore();
-
-  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
-    next({ name: 'login' });
-  } else if (to.meta.requiresAdmin && !authStore.isAdmin) {
-    next({ name: 'admin-login' });
-  } else {
-    next();
-  }
 });
 
 export default router;
