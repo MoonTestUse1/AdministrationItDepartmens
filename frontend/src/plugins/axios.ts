3def094a5d97270383198332f63a3f9fe08138c2
MoonTestUse1 <crocoman7887@gmail.com>
2025-01-05 00:48:39 +0600

    тестd32

--- frontend/src/plugins/axios.ts
+++ frontend/src/plugins/axios.ts
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: '/api',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
@@ -35,7 +34,4 @@ axiosInstance.interceptors.response.use(
   }
 )
 
-export default axiosInstance 
-=======
- 
->>>>>>> my-feature-branch
+export default axiosInstance
